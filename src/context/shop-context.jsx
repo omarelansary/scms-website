f9f248da52067f2f3050cffab84e37870166adf7
omarelansary <73857229+omarelansary@users.noreply.github.com>
2024-08-18 03:53:36 +0300

    feat: cart page

--- src/context/shop-context.jsx
+++ src/context/shop-context.jsx
@@ -13,7 +13,7 @@ const getDefaultCart = () => {
 
 export const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState(getDefaultCart());
-
+  
   const getTotalCartAmount = () => {
     let totalAmount = 0;
     for (const item in cartItems) {
@@ -23,6 +23,54 @@ export const ShopContextProvider = (props) => {
       }
     }
     return totalAmount;
+  };
+
+  const generateUniqueOrderID = () => {
+    return `${Date.now()}-${Math.floor(Math.random() * 10000)}`;
+  };
+
+  const placeOrder = (name,email,address) => {
+    let totalAmount = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+
+        let itemInfo = productsData.find((product) => product.id === Number(item));
+        totalAmount += cartItems[item] * itemInfo.price;
+      }
+    }
+    var order={
+      "orderID": generateUniqueOrderID(),
+      "orderStatus": "ordered",
+      "orderTotalAmount": totalAmount,
+      "ordererName": name,
+      "ordererAddress": email,
+      "ordererEmail": address,
+      "orderItems": {cartItems},
+    }
+
+    let orders = JSON.parse(localStorage.getItem('orders')) || [];
+    orders.push(order);
+    localStorage.setItem('orders', JSON.stringify(orders));
+  };
+
+  const getOrdersFromLocalStorage = () => {
+    const orders = localStorage.getItem('orders');
+    if (orders) {
+      return JSON.parse(orders);
+    } else {
+      return [];
+    }
+  };
+
+  const removeOrderFromLocalStorage = (orderID) => {
+    // Retrieve the orders
+    let orders = getOrdersFromLocalStorage();
+  
+    // Filter out the order with the given ID
+    orders = orders.filter(order => order.orderID !== orderID);
+  
+    // Save the updated orders back to local storage
+    localStorage.setItem('orders', JSON.stringify(orders));
   };
 
   const addToCart = (itemId) => {
@@ -37,16 +85,25 @@ export const ShopContextProvider = (props) => {
     setCartItems((prev) => ({ ...prev, [itemId]: newAmount }));
   };
 
-  const checkout = () => {
+  const getCartItemsCount = () => {
+    return Object.values(cartItems).reduce((total, count) => total + count, 0);
+  };
+
+  const checkout = (name,email,address) => {
+    placeOrder(name,email,address);
+    //removeOrderFromLocalStorage("1723811914884-6369")
     setCartItems(getDefaultCart());
+    console.error(getOrdersFromLocalStorage())
   };
 
   const contextValue = {
     cartItems,
     addToCart,
+    getOrdersFromLocalStorage,
     updateCartItemCount,
     removeFromCart,
     getTotalCartAmount,
+    getCartItemsCount,
     checkout,
   };
 
